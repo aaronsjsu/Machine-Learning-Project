4a156aa635cd04f2012559f89e3f9125b700c4b1
aaronsjsu <aaron.smith@sjsu.edu>
2019-11-03 22:36:07 -0800

    simplified code

--- Ciphers/shift_cipher.py
+++ Ciphers/shift_cipher.py
@@ -18,28 +18,20 @@ result = "" # Store the result in this string
 
 file_input = open(input_file_name, "r")
 file_output = open(output_file_name, "w")
-# print("Shifting by " + str(key))
+print("Shifting by " + str(key))
 
-
-# Loop until the result string is the desired length
-while len(result) != text_length:
-    c = file_input.read(1).lower() # Reads 1 character at a time
-    # c will already be lowercase
-    ascii_code = ord(c) # Returns the ascii code of the character c
-    # First eliminate spaces
-    if (c == ' '):
-        c = ''
-    # Then eliminate non-letter characters
-    elif (ascii_code < 97 or ascii_code > 122):
-        c = ''
-    # Else, shift the valid character by the amount in key
-    else:
-        c = ascii_code + key
-        if (c > 122):
-            c -= 26
-        c = chr(c) # Convert ascii code back to character
-    result += c # Add our character to our result
-
+# Read plaintext line by line. Break once result string is desired length.
+for line in file_input:
+	for char in line:
+		if char.isalpha(): # True if char is a letter (i.e. no special characters, no spaces, etc.)
+			char = ord(char.lower()) + key # Convert character to ascii code. Shift by key amount.
+			if (char > 122):
+				char -= 26
+			result += chr(char) # Convert ascii code back to character. Add to result.
+			if (len(result) == text_length):
+				break
+	if (len(result) == text_length):
+		break
 
 # Write result to our output file
 file_output.write(result)
