1192a7cce6514b50127459c832428dccb240e93f
aaronsjsu <aaron.smith@sjsu.edu>
2019-11-03 23:16:52 -0800

    updated to run many times

--- Ciphers/shift_cipher.py
+++ Ciphers/shift_cipher.py
@@ -11,31 +11,38 @@ __date__ = "10/13/2019"
 import random
 
 input_file_name = "brown_corpus.txt"
-output_file_name = "result.txt"
-text_length = 1000 # Length of the character sequence to encrypt
-key = random.randint(1, 25) # The amount to shift by
-result = "" # Store the result in this string
+output_file_name = "../Data/Shift Cipher/text_length_100.txt"
+text_length = 100 # Length of the character sequence to encrypt
+iters = 10000 # How many times to encrypt a string
 
-file_input = open(input_file_name, "r")
 file_output = open(output_file_name, "w")
-print("Shifting by " + str(key))
+for i in range(iters):
+	key = random.randint(1, 25) # The amount to shift by
+	result = "" # Store the result in this string
 
-# Read plaintext line by line. Break once result string is desired length.
-for line in file_input:
-	for char in line:
-		if char.isalpha(): # True if char is a letter (i.e. no special characters, no spaces, etc.)
-			char = ord(char.lower()) + key # Convert character to ascii code. Shift by key amount.
-			if (char > 122):
-				char -= 26
-			result += chr(char) # Convert ascii code back to character. Add to result.
-			if (len(result) == text_length):
-				break
-	if (len(result) == text_length):
-		break
+	file_input = open(input_file_name, "r")
 
-# Write result to our output file
-file_output.write(result)
+	#print("Shifting by " + str(key))
 
-# Lastly, close our files
-file_input.close()
+	# Start our reading from a relatively random line from within the input file.
+	random_line = random.randint(1, 45000)
+	#print("Reading from file at line " + str(random_line))
+	for j in range(random_line):
+		file_input.readline()
+	# Read plaintext line by line. Break once result string is desired length.
+	while (len(result) != text_length):
+		line = file_input.readline()
+		for char in line:
+			if char.isalpha(): # True if char is a letter (i.e. no special characters, no spaces, etc.)
+				char = ord(char.lower()) + key # Convert character to ascii code. Shift by key amount.
+				if (char > 122):
+					char -= 26
+				result += chr(char) # Convert ascii code back to character. Add to result.
+				if (len(result) == text_length):
+					break
+
+	# Write result to our output file
+	file_output.write(result + "\n")
+	file_input.close()
+
 file_output.close()
