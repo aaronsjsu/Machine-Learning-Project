a6f36e05bcbc133005e2cf6bdfd2dd815572b6b7
aaronsjsu <aaron.smith@sjsu.edu>
2019-11-03 22:19:03 -0800

    Simplified code and made more readable

--- Ciphers/vigenere_cipher.py
+++ Ciphers/vigenere_cipher.py
@@ -1,45 +1,43 @@
 """
-This file is used to implement a vigenere cipher. It works by
-reading input from a specified txt file, filtering the characters to be
-lowercase letters (discarding non-letter characters), and then encrypting
-the characters using the vigenere cipher. See
-https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher
-for a brief description. The result is then written to a specified txt file.
-__author__ = "Zizhen Huang"
-__date__ = "10/28/2019"
+This file is used to implement a vigenere cipher. It works by reading input
+from a specified txt file, filtering the characters to be lowercase letters
+(discarding non-letter characters), and then encrypting the characters using
+the vigenere cipher. See https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher
+for a brief description of the vigenere cipher. The result is then written to
+a specified txt file.
+
+__author__ = "Zizhen Huang and Aaron Smith"
+__date__ = "11/3/2019"
 """
 
 
 input_file_name = "brown_corpus.txt"
 output_file_name = "result.txt"
-key = "VIGENERECIPHER" # length 10 and serves as row in the table
+vigenere_table_file = "vigenere_table.txt"
+text_length = 1000 # Length of the character sequence to encrypt
+key = "VIGENERECIPHER" # Key length 10
 result = "" # Store the result in this string
 plaintext = "" # Plaintext read from the input file
 
 file_input = open(input_file_name, "r")
 file_output = open(output_file_name, "w")
 
-# remember we just wnat to lower case and letters
-# create a The Vigenère square or Vigenère table,
-# by reading the data from vigenere_table.txt 
-with open("vigenere_table.txt") as vigenere_table_data:
-	Vigenère_square = [line.split() for line in vigenere_table_data]
+# Read data from vigenere_table_file and place it in a 2d list.
+# This matrix represents all the possible caesar chiphers. The key
+# is then used to decide which shift to use to encrypt a character.
+with open(vigenere_table_file) as vigenere_file_data:
+	vigenere_matrix = [line.split() for line in vigenere_file_data]
 
-# another way here is to read the whole file into plaintext and traverse
-# here we just read line by line and that is why we make index_for_key global
-index_for_key = 0
-for plaintext in file_input:
-	for char in plaintext:
-		if char.isalpha():
-			index_for_key %= len(key) # rotate the key w.r.t index_for_key
-			row = ord(key[index_for_key]) - 65 # get the row 
-			index_for_key += 1
-			if char.islower():				
-				col = (ord(char) - 97) # get the column
-				result += Vigenère_square[row][col] # get the encryption char from the table
-			elif char.isupper():				
-				col = (ord(char) - 65)
-				result += Vigenère_square[row][col]
+# Read plaintext line by line
+key_index = 0 # Represents current index/position in the key
+for line in file_input:
+	for char.lower() in line:
+		if char.isalpha(): # True if char is a letter (i.e. no special characters, no spaces, etc.)
+			key_index %= len(key) # Keep index in bounds of the length of the key
+			row = ord(key[key_index]) - 65 # ord() function gets ascii value. row represents row in matrix
+			col = (ord(char) - 97) # Get the column in our matrix
+			result += vigenere_matrix[row][col] # Get the encrypted character from the matrix
+			key_index += 1 # Increment our index.
 
 # Write result to our output file
 file_output.write(result)
@@ -47,9 +45,3 @@ file_output.write(result)
 # Lastly, close our files
 file_input.close()
 file_output.close()
-
-
-
-
-
-
